b9f170482a4397fb93e273dbc2fe13abc1b167b1
henrtitb <henri.backlund@metropolia.fi>
2014-03-27 13:14:02 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
-
 <?php
+
 
 include_once 'db_connect.php';
 include_once 'functions.php';
@@ -19,12 +19,13 @@ include_once 'functions.php';
 <html>
         <head>
                 <title> Log In </title>
-                <script type="text/javascript" src="sha512.js"></script>
-                <script type="text/javascript" src="forms.js"></script>
-                <link rel="stylesheet" type="text/css" href="style.css"/>
+                <script type="text/javascript" src="js/sha512.js"></script>
+                <script type="text/javascript" src="js/forms.js"></script>
+                <link href="css/bootstrap.min.css" rel="stylesheet">
+				<link href="css/login.css" rel="stylesheet">
         </head>
         <body>
-                <?php
+            <?php
 				require_once('process_login.php');
 				require_once('db_connect.php');
 				require_once('functions.php');
@@ -39,10 +40,13 @@ include_once 'functions.php';
 				} else{
 					echo 'You are not authorized to access this page, please login. <br/>';
 				}
-                ?>
-                <form action="process_login.php" method="post" name="login_form">
-                Email: <input type="text" name="email" /> <br/>
-                Password: <input type="password" name="password" id="password" /> <br/>
-                <input type="button" value="Kirjaudu sisään" onclick="formhash(this.form, this.form.password);" />
+                ?> 
+                <form class="form-signin" action="process_login.php" method="post" name="login_form">
+				<h2 class="form-signin-heading">Kirjaudu sisään</h2>
+                Email: <input type="text" name="email" class="form-control" placeholder="Sähköpostiosoite"  /> <br/>
+                Password: <input type="password" name="password" class="form-control" placeholder="Salasana" id="password" /> <br/>
+                <input class="btn btn-lg btn-primary btn-block" type="button" value="Kirjaudu sisään" onclick="formhash(this.form, this.form.password);" />
+				<a href="#">Unohtuiko salasana?</a>
+				</form>
         </body>
 </html>
