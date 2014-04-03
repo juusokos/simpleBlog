c08ca2c179b62bac9ead23716b44ac2379956948
juusokos <juuso.koskenseppa@metropolia.fi>
2014-04-03 15:02:18 +0300

    Update prokkis.sql

--- prokkis.sql
+++ prokkis.sql
@@ -14,25 +14,14 @@ CREATE TABLE `juusokos`.`simple_login_attempts`(
 
 CREATE TABLE `juusokos`.`simple_themes`(
 	`ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`theme_name` VARCHAR(30) NOT NULL,
-	`background_color` VARCHAR(6) NOT NULL,
-	`font_color` CHAR(6) NOT NULL
+	`theme_url` VARCHAR(60) NOT NULL,
+	`font` CHAR(60) NOT NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE `juusokos`.`simple_images`(
+CREATE TABLE `juusokos`.`simple_banner`(
 	`ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`url` VARCHAR(30) NOT NULL
-) ENGINE = InnoDB;
-
-CREATE TABLE `juusokos`.`simple_posts`(
-	`ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	site_ID` int(11) NOT NULL,
-	FOREIGN KEY (site_ID) references simple_sites(ID),
-	`title` VARCHAR(30) NOT NULL,
-	`image_ID` int(11) NOT NULL,
-	FOREIGN KEY (image_ID) references simple_images(ID),
-	`content` VARCHAR(500) NOT NULL,
-	`date` DATE NOT NULL
+	`banner_url` VARCHAR(60) NOT NULL,
+	`banner_url_thumb` VARCHAR(60) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE `juusokos`.`simple_sites`(
@@ -41,9 +30,23 @@ CREATE TABLE `juusokos`.`simple_sites`(
 	FOREIGN KEY (user_ID) references simple_users(ID),
 	`theme_ID` int(11) NOT NULL,
 	FOREIGN KEY (theme_ID) references simple_themes(ID),
-	`post_ID` int(11) NOT NULL,
-	FOREIGN KEY (post_ID) references simple_posts(ID)
+	`banner_ID` int(11) NOT NULL,
+	FOREIGN KEY (banner_ID) references simple_banner(ID),
+	`about` VARCHAR(300) NOT NULL,
+	`blog_title` VARCHAR(50) NOT NULL,
+	`blog_description` VARCHAR(100) NOT NULL
 ) ENGINE = InnoDB;
+
+CREATE TABLE `juusokos`.`simple_posts`(
+	`ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`site_ID` int(11) NOT NULL,
+	FOREIGN KEY (site_ID) references simple_sites(ID),
+	`title` VARCHAR(30) NOT NULL,
+	`image_url` VARCHAR(60) NOT NULL,
+	`content` VARCHAR(500) NOT NULL,
+	`date` DATE NOT NULL
+) ENGINE = InnoDB;
+
 
 INSERT INTO `juusokos`.`simple_users` VALUES(
 	, 
@@ -52,3 +55,28 @@ INSERT INTO `juusokos`.`simple_users` VALUES(
 	'QBsJ6rPAE9TKVJIruAK+yP1TGBkrCnXyAdizcnQpCA+zN1kavT5ERTuVRVW3oIEuEIHDm3QCk/dl6ucx9aZe0Q==',
 	'401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429080fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1'
 );
+
+INSERT INTO `juusokos`.`simple_themes` VALUES(
+	,
+	`theme_url`,
+	`font`
+);
+
+INSERT INTO `juusokos`.`simple_sites` VALUES(
+	1,
+	3,
+	1,
+	1,
+	'Olen paras ja asdfjasfjkafejhafsjk',
+	'Paras Blogi',
+	'Paras Blogi kertoo parhaista ashjasfjihfadjhafdj aisoista'
+);
+
+INSERT INTO `juusokos`.`simple_posts` VALUES(
+	1,
+	1,
+	'Paras Posti',
+	'http://users.metropolia.fi/~juusokos/Untitled-1.jpg',
+	'liirumlaarumia',
+	'2014-02-02' 
+); 
