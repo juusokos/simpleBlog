e5dd236b127798abf04bc5568560e032c5d6efe7
juusokos <juuso.koskenseppa@metropolia.fi>
2014-04-03 14:54:53 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,23 @@
 
 
-
 <?php
 include_once 'psl-config.php';   // As functions.php is not included
 $mysqli = new mysqli("mysql.metropolia.fi", "user", "salasana", "database");
+
+$host = HOST;
+$dbname = DATABASE;
+$user = USER;
+$pass = PASSWORD;
+
+try {
+	$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+	$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$DBH->query("SET NAMES utf8");
+}
+catch(PDOException $e) {
+	echo "Could not connect to database.";
+	//file_put_contents('../../loki/PDOErrors.txt', $e->getMessage() . "\n", FILE_APPEND);
+}
 ?>
 
 
