670bff59b9f84aa06e963729af352d3060b9b51c
nelliholopainen <nelli.holopainen@gmail.com>
2014-04-18 15:48:13 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -12,5 +12,5 @@ $params = session_get_cookie_params();
 setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 // Destroy session
 session_destroy();
-header('Location: ./blogisivu.php?id='.$url);
+header('Location: ./index.php);
 ?>
