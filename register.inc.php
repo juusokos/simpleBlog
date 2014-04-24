90fa9460841af1c18184066a11abdf4967bc3587
juusokos <juuso.koskenseppa@metropolia.fi>
2014-04-24 14:41:55 +0300

    Update register.inc.php

--- register.inc.php
+++ register.inc.php
@@ -81,8 +81,8 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p'])) {
 				$blog_title = 'Uusi Simple Blogi';
 				$blog_description = 'Kirjoita halutessasi blogillesi kuvaus';
 				
-				$luoBlogi = $mysqli->prepare("INSERT INTO simple_sites (user_ID, theme_ID, font_ID, about, blog_title, blog_description) VALUES (?, ?, ?, ?, ?)");
-				$luoBlogi->bind_param('iisss', $user_id, $theme_id, $font_id, $about, $blog_title, $blog_description);
+				$luoBlogi = $mysqli->prepare("INSERT INTO simple_sites (user_ID, theme_ID, font_ID, about, blog_title, blog_description) VALUES (?, ?, ?, ?, ?, ?)");
+				$luoBlogi->bind_param('iiisss', $user_id, $theme_id, $font_id, $about, $blog_title, $blog_description);
 				$luoBlogi->execute();
 				$luoBlogi->close();
 				
