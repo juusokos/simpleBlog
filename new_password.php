d9ad19f3ea5ece00a1ee50e0a825381eb3302f39
nelliholopainen <nelli.holopainen@gmail.com>
2014-04-28 17:23:00 +0300

    Update new_password.php

--- new_password.php
+++ new_password.php
@@ -4,9 +4,8 @@ include_once 'psl-config.php';
  
 $error_msg = "";
  
-if (isset($_POST['username'])) {
+if (isset($_POST['email'],$_POST['p'])) {
     // Sanitize and validate the data passed in
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -15,7 +14,7 @@ if (isset($_POST['username'])) {
     }
 	
 	
-   // $password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
+   $password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
     if (strlen($password) != 128) {
         // The hashed pwd should be 128 characters long.
         // If it's not, something really odd has happened
@@ -53,7 +52,7 @@ if (isset($_POST['username'])) {
         $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
  
         // Create salted password 
-        $password = hash('sha512', $new_pass . $random_salt);
+        $password = hash('sha512', $password . $random_salt);
 		
 		
         // Insert the new user into the database 
